41d9d5496e42f6286b17c7d2b96c950d94124f40
Hintee <dan.hintea@ebriza.com>
2020-02-04 15:00:04 +0200

    OD: Scaffold solution

---
+++ H.Playground.NancyWebApi.SelfHost/H.Playground.NancyWebApi.Core/HealthModule.cs
@@ -0,0 +1,18 @@
+﻿using Nancy;
+using System;
+
+namespace H.Playground.NancyWebApi.Core
+{
+    public class HealthModule : NancyModule
+    {
+        public HealthModule() : base("/health")
+        {
+            Get(
+                path: "/ping",
+                action: _ => $"Pong @ {DateTime.Now}",
+                condition: null,
+                name: null
+            );
+        }
+    }
+}
---
+++ H.Playground.NancyWebApi.SelfHost/H.Playground.NancyWebApi.SelfHost/Program.cs
@@ -0,0 +1,49 @@
+﻿using Mono.Unix;
+using Mono.Unix.Native;
+using Nancy.Hosting.Self;
+using System;
+
+namespace H.Playground.NancyWebApi.SelfHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string url = "http://localhost:8888";
+
+            Console.WriteLine($"Starting Nancy on {url}...");
+
+            var uri = new Uri(url);
+            var host = new NancyHost(uri);
+            host.Start();
+
+            if (IsRunningOnMono())
+            {
+                var terminationSignals = GetUnixTerminationSignals();
+                UnixSignal.WaitAny(terminationSignals);
+            }
+            else
+            {
+                Console.ReadLine();
+            }
+
+            host.Stop();
+        }
+
+        private static bool IsRunningOnMono()
+        {
+            return Type.GetType("Mono.Runtime") != null;
+        }
+
+        private static UnixSignal[] GetUnixTerminationSignals()
+        {
+            return new[]
+            {
+                new UnixSignal(Signum.SIGINT),
+                new UnixSignal(Signum.SIGTERM),
+                new UnixSignal(Signum.SIGQUIT),
+                new UnixSignal(Signum.SIGHUP)
+            };
+        }
+    }
+}
